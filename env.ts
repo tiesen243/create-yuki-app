9609632b2625333f94e40f1814f38a12214bb1f5
tiesen243 <ttien56906@gmail.com>
2025-05-26 22:08:47 +0700

    chore: minor changes

--- env.ts
+++ env.ts
@@ -23,6 +23,7 @@ export const env = createEnv({
   client: {},
 
   experimental__runtimeEnv: process.env,
+
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
